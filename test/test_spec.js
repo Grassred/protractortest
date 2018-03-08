3e214b143731dace291ac361ee11430089a3bc5a
admin <admin>
2018-03-08 22:37:26 +0200

    k

--- test/test_spec.js
+++ test/test_spec.js
@@ -4,15 +4,15 @@ describe('Protractor Demo Page', function () {
       browser.get('https://glitchitsystem.com/protractor-demo')
     })
   
-    // it('should fail', function () {
-    //   browser.ignoreSynchronization = true
-    //   browser.get('https://glitchitsystem.com/protractor-demo')
-    //   var name = 'Glitchy'
-    //     element(by.id('nameFld')).sendKeys(name)
+    it('should fail', function () {
+      browser.ignoreSynchronization = true
+      browser.get('https://glitchitsystem.com/protractor-demo')
+      var name = 'Glitchy'
+        element(by.id('nameFld')).sendKeys(name)
   
-    //           element(by.id('submitBtn')).click()
+              element(by.id('submitBtn')).click()
   
-    //   expect(element(by.id('welcomeText')).getText()).toEqual('Welcome to Protractor ' + name + '!')
-    // })
+      expect(element(by.id('welcomeText')).getText()).toEqual('Welcome to Protractor ' + name + '!')
+    })
   })
  
